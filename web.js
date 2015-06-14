eb54638ae54268f52f013eafe2263515bdb02244
Billiam <billiamthesecond@gmail.com>
2015-06-13 19:29:09 -0500

    Adding steam integration

--- web.js
+++ web.js
@@ -2,6 +2,9 @@ var ack = require('ac-koa').require('hipchat');
 var pkg = require('./package.json');
 var app = ack(pkg);
 
+var Notifier = require('ac-koa-hipchat-notifier').Notifier;
+var Store = require('./lib/store');
+
 var addon = app.addon()
   .hipchat()
   .allowRoom(true)
@@ -10,9 +13,16 @@ var addon = app.addon()
 if (process.env.DEV_KEY) {
   addon.key(process.env.DEV_KEY);
 }
- 
-addon.webhook('room_message', /^\/hello$/, function *() {
-  yield this.roomClient.sendNotification('Hi, ' + this.sender.name + '!');
+
+var notifier = Notifier({format: 'html', dir: __dirname + '/messages'});
+var store = Store(notifier);
+
+addon.webhook('room_message', /http:\/\/store.steampowered.com\/app\/(\d+)/,  function *() {
+  var match = this.match;
+  console.log(match);
+  if(match && match[1]) {
+    yield store.fetchGameInfo(match[1]);
+  }
 });
- 
+
 app.listen();
